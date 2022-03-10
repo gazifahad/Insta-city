03317092df55af019b165ef2c7349f4ff0565b64
gazifahad <gazifahad3@gmail.com>
2022-03-11 02:48:23 +0600

    liked post repitation problem solved

--- js/app.js
+++ js/app.js
@@ -40,6 +40,8 @@ const switchTab = (id) => {
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
     } else if (id === "liked") {
+      // 6th bug 
+        document.getElementById( "liked" ).textContent='';
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
